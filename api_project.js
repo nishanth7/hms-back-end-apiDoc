fcf08b06d023fcfb9609a371444f18c5f241ee26
nishanth <n.ganesh@everteam.com>
2024-12-06 13:25:59 +0530

    First commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "My API Documentation",
+  "version": "1.0.0",
+  "description": "Documentation for my API",
+  "title": "My API Docs",
+  "url": "http://localhost:3000/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2024-12-05T18:30:43.628Z",
+    "url": "https://apidocjs.com",
+    "version": "0.28.1"
+  }
+});
